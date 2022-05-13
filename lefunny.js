d8cfa59ac83e80df672d32943f0d637e8d5b97cd
marinayubicraft <marinayubicraft@gmail.com>
2022-05-13 19:58:20 +0200

    max

--- lefunny.js
+++ lefunny.js
@@ -1,4 +1,4 @@
 const max = 36
 const img = document.getElementById("lefunny")
-const r = Math.floor(Math.random() * 36) + 1;
+const r = Math.floor(Math.random() * max) + 1;
 img.src = `banners/b${r}.png`
