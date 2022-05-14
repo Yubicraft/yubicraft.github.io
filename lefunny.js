4bfab5933934032e1a4e70bf0335f39c1af42ff4
marinayubicraft <marinayubicraft@gmail.com>
2022-05-14 10:47:09 +0200

    random when click

--- lefunny.js
+++ lefunny.js
@@ -1,4 +1,9 @@
 const max = 65
 const img = document.getElementById("lefunny")
-const r = Math.floor(Math.random() * max) + 1;
-img.src = `banners/b${r}.png`
+const selectRandom = () => {
+    const r = Math.floor(Math.random() * max) + 1;
+    img.src = `banners/b${r}.png`
+}
+img.addEventListener("click", selectRandom)
+selectRandom()
+
